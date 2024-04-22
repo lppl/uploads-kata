b3195025e0c865bec5ee08d17508142681a53297
Łukasz Pietrek <lpietrek@gmail.com>
2024-04-22 13:11:22 +0200

    Make uploads form great again

--- uploads-frontend/src/components/UploadForm.tsx
+++ uploads-frontend/src/components/UploadForm.tsx
@@ -18,20 +18,44 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import * as React from "react";
+
+const allowedFileMimes = [
+  "image/png",
+  "image/jpeg",
+  "image/bmp",
+  "image/tiff",
+  "image/webp",
+];
 
 const formSchema = z.object({
   userName: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
   userEmail: z.string().email({ message: "Provide valid email" }),
+  file: z.instanceof(File).superRefine((file: File, ctx) => {
+    if (file.size > 5000000) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: `Maximum file size (...). Max is 5MB`,
+        fatal: true,
+      });
+    } else if (!allowedFileMimes.includes(file.type)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: `Unsupported filetype. Please use one of ${allowedFileMimes.join()}`,
+        fatal: true,
+      });
+    }
+  }),
 });
 
 export function UploadForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      userName: "",
-      userEmail: "",
+      userName: "Fooobar Name",
+      userEmail: "foobar@dummy.com",
     },
   });
 
@@ -79,6 +103,32 @@ export function UploadForm() {
                 </FormItem>
               )}
             />
+
+            <FormField
+              control={form.control}
+              name="file"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel className="text-xl">File</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="file"
+                        ref={field.ref}
+                        name={field.name}
+                        onBlur={field.onBlur}
+                        disabled={field.disabled}
+                        onChange={(e) => {
+                          form.setValue("file", e.target.files[0]);
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+
             <Button type="submit">Upload</Button>
           </form>
         </Form>
