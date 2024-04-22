fff446e2c330ce3cc9c16dcab30f6d7674049ad0
Łukasz Pietrek <lpietrek@gmail.com>
2024-04-22 19:07:11 +0200

    Initiate uploads controller

---
+++ uploads-backend/app/Http/Controllers/Uploads.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use http\Exception\RuntimeException;
+use Illuminate\Http\Request;
+
+class Uploads extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        throw new RuntimeException("Not implemented");
+    }
+}
--- uploads-backend/routes/web.php
+++ uploads-backend/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources([
+    'uploads' => \App\Http\Controllers\Uploads::class,
+]);
+
